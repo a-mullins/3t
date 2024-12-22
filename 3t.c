b7a78825183e8c5b9103e39596651bfacb04ab47
Adam Mullins <adam.l.m@gmail.com>
2024-12-22 04:44:42 -0500

    hacking

--- 3t.c
+++ 3t.c
@@ -1,252 +1,149 @@
-#include <errno.h>     // errno
-#include <fcntl.h>     // open()
-#include <signal.h>    // kill, signal()
-#include <stdio.h>     // fprintf(), getchar(), stdin, out, err
-#include <stdlib.h>    // atexit(), exit()
-#include <string.h>    // strerror()
-#include <sys/ioctl.h> // ioctl()
-#include <termios.h>   // tcgetattr()
-#include <time.h>      //
-#include <unistd.h>    // tcgetattr(), getpid()
+#include <locale.h>
+#include <ncurses.h>
+#include <unistd.h>  // for usleep()
+#include <stdlib.h>  // for rand()
+#include <time.h>    // for time()
 
+#define FLAKES 256
 
-
-#define LF                    "\012"
-#define CR                    "\015"
-#define RESET_TERM            "\033c"
-// CSI = Control Sequence Introducer
-#define CSI                   "\033["
-#define CLEAR_SCREEN      CSI "2J"
-#define CLEAR_ALL         CSI "3J"
-#define HIDE_CURSOR       CSI "?25l"
-#define SHOW_CURSOR       CSI "?25h"
-#define GET_SCREEN_PIXELS CSI "14t"
-#define GO_HOME           CSI "H"
-
-// fatal error printf
-#define FEPRINTF(msg)                           \
-    fprintf(stderr,                             \
-            "ERR: %s:%d %s\n",                  \
-            __FILE__, __LINE__, msg);           \
-    exit(1);                                    \
-
-
-// struct screen {
-//     int rows;
-//     int cols;
-//     size_t buf_size;
-//     char *buf;
-// };
-
-//static int tty_fd = 0;
-// static struct termios old_term = {0};
-// static struct screen screen = {0};
-
-
-// void on_win_change();
-// void restore_term();
-// void draw(unsigned int x, unsigned int y, char c);
-// void draw_str(unsigned int x, unsigned int y, const char *s);
-
-
-// void write_out() {
-//     printf("%s", CLEAR_SCREEN);
-//     printf("%s", CSI "H");
-//     for(unsigned int row = 0; row < screen.rows; row++) {
-//         write(1, screen.buf + row*screen.cols, screen.cols);
-//         //memcpy(line_buf, screen.buf + row*screen.cols, screen.cols);
-//         //snprintf(line_buf, size, "%s", screen.buf + row*screen.cols);
-//         // for(unsigned int col = 0; col < screen.cols; col++) {
-//         //     putchar(*(screen.buf + row * screen.cols + col));
-//         // }
-//         //printf("%s", line_buf);
-//         if(row == screen.rows) {
-//             printf("\r");
-//         } else {
-//             printf("\n");
-//         }
-//     }
-//     //printf("screen rows: %d", screen.rows);
-//     // for(unsigned short i = 0; i < screen.cols; i++) {
-//     //     putchar('.');
-//     // }
-//     printf("%s", CSI "H");
-//     fflush(stdout);
-// }
-
-
-// void draw(unsigned int x, unsigned int y, char c) {
-//     if( x < screen.cols && y < screen.cols ) {
-//         *(screen.buf + y * screen.cols + x) = c;
-//     }
-// }
-
-
-// void draw_str(unsigned int x, unsigned int y, const char *s) {
-//     char *target = screen.buf + y*screen.cols + x;
-//     // could use strcpy, but we don't want the null.
-//     memcpy(target, s, strlen(s));
-// }
-
-
-// void on_win_change() {
-//     // TODO get screen pixels for more accurate aspect ratio
-//     struct winsize ws = {0};
-
-//     if(ioctl(tty_fd, TIOCGWINSZ, &ws) == -1) {
-//         FEPRINTF("ioctl to get win size failed");
-//     }
-
-//     // size_t old_size = screen.buf_size;
-//     // size_t new_size = sizeof (char) * (size_t)ws.ws_row * (size_t)ws.ws_col;
-//     // if(old_size < new_size) {
-//     //     // this next call will overwrite screen.buf with NULL if the alloc fails,
-//     //     // so in theory it would be nice to keep a copy of the old pointer and
-//     //     // restore it in that case.
-//     //     // but since we can't continue without a screen buffer, we will just die.
-//     //     screen.buf = realloc(screen.buf, new_size);
-//     //     if(screen.buf == NULL) {
-//     //         FEPRINTF("could not realloc memory for screen buffer");
-//     //     }
-//     //     screen.buf_size = new_size;
-
-//     //     // fill the newly allocated memory with safe characters.
-//     //     // we wouldn't want accidentally print 2000 BEL. :-|
-//     //     memset(screen.buf + old_size, ' ', new_size - old_size);
-//     // }
-
-//     screen.rows = ws.ws_row;
-//     screen.cols = ws.ws_col;
-// }
-
-
-int main(int argc, char **argv) {
-    // screen.buf_size = (size_t)1920;
-    // screen.buf = malloc(1920 * sizeof (char));
-    // memset(screen.buf, ' ', 1920);;
-
-    // get fd for term we are connected to
-    int tty_fd = open("/dev/tty", O_NONBLOCK);
-    if(tty_fd == -1) {
-        FEPRINTF("open(/dev/tty, ...)");
-    }
-
-    // // hook window resizes
-    // if(signal(SIGWINCH, on_win_change) == SIG_ERR) {
-    //     FEPRINTF("signal(SIGWINCH, ...)");
-    // }
-
-    static struct termios old_term;
-    static struct termios new_term;
-    // save current terminal configuration, so that it can be restored on exit
-    if(tcgetattr(0, &old_term)) {
-        FEPRINTF("tcgetattr(0, ...)");
-    }
-    // set up new terminal configuration
-    new_term = old_term;
-    // disable canonical mode and local echo
-    new_term.c_lflag &= (~ICANON & ~ECHO);
-    if(tcsetattr(0, TCSANOW, &new_term)) {
-        FEPRINTF("tcsetattr(...)");
-    }
-    getchar(); // pause for attachment by gdb. :-)
-    //puts(HIDE_CURSOR);
-
-    struct winsize ws = {0};
-    if(ioctl(tty_fd, TIOCGWINSZ, &ws) == -1) {
-        FEPRINTF("ioctl to get win size failed");
-    }
-
-    // size_t buf_size = (ws.ws_row+1)*(ws.ws_col+1);
-    // char *buf = calloc(buf_size, sizeof (char));
-    // memset(buf, ' ', buf_size);
-    char buf[ws.ws_row][ws.ws_col];
-    memset(buf, ' ', ws.ws_row * ws.ws_col);
-    for(unsigned short row = 0; row < ws.ws_row; row++) {
-        for(unsigned short col = 0; col < ws.ws_col; col++) {
-            if      (col   == 0)         buf[row][col] = (char)row%10+0x30;
-            else if (col+1 == ws.ws_col) buf[row][col] = 'E';
-            //else                         buf[row][col] = '.';
-        }
-    }
-
-    fputs(RESET_TERM, stdout);
-
+int
+main()
+{
+    // seed random
     srand(time(NULL));
+    // set locale, per the advice of `man ncurses`. by passing "" as
+    // second arg, we follow whatever is set by the env.
+    setlocale(LC_ALL, "");
 
-    // GAME LOOP
-    float target_fps = 48.0;
-    struct timespec tm_asked = { .tv_sec = 0, .tv_nsec = (long)(1e9 * 1.0/target_fps)};
-    struct timespec tm_left  = { 0 };
-    struct timespec tm_stamp_start = { 0 };
-    struct timespec tm_stamp_now = { 0 };
-    clock_gettime(CLOCK_REALTIME, &tm_stamp_start);
-    unsigned long tm_diff_sec = 0;
-    unsigned int frame_cnt = 0;
-    char cnt_buf[128] = {0};
-    char hi_spinner[] = "               __--==^^==--__               ";
-    char md_spinner[] = "        __--==^              ^==--__        ";
-    char lo_spinner[] = "__--==^^                            ^^==--__";
-    while(1) {
-        fputs(GO_HOME, stdout); fflush(stdout);
-        fputs(CSI "J", stdout); fflush(stdout);
+    // INITIALIZE NCURSES
+    initscr();
+    start_color();
 
-        clock_gettime(CLOCK_REALTIME, &tm_stamp_now);
-        tm_diff_sec = (tm_stamp_now.tv_sec - tm_stamp_start.tv_sec);
+    // cbreak disables line-buffering, input is available immediately.
+    cbreak();
+    noecho();
+    // hide cursor
+    curs_set(0);
+    // do not scroll if the cursor falls off the last line.
+    scrollok(stdscr, FALSE);
+    // if TRUE, flush on interrupt. Feels faster, but confuses
+    // ncurses, according to man cur_inopts.
+    intrflush(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+    // nodelay makes getch() nonblocking, which will want later.
+    // nodelay(stdscr, TRUE);
 
-        // flowers
-        unsigned int fl_row = (rand() % ws.ws_row);
-        unsigned int fl_col = 1 + (rand() % (ws.ws_col - 2));
-        char fl_c;
-        switch(buf[fl_row][fl_col]) {
-        case ' ':
-            fl_c = '.';
-            break;
-        case '.':
-            fl_c = '_';
-            break;
-        case '_':
-            fl_c = '|';
-            break;
-        case '|':
-            fl_c = ' ';
-            break;
-        }
-        buf[fl_row][fl_col] = fl_c;
+    int y_max, x_max, y_max_old, x_max_old;
+    getmaxyx(stdscr, y_max, x_max);
+    y_max_old = 0;
+    x_max_old = 0;
 
-        snprintf(cnt_buf, 40, "%c target fps: %2.1f",
-                 hi_spinner[frame_cnt % (sizeof (hi_spinner) - 1)],
-                 target_fps);
-        memcpy(&buf[1][8], cnt_buf, strlen(cnt_buf));
+    // snowflakes
+    struct snowflake {
+        int x;
+        int y;
+        int last_moved;
+        int stuck;
+    };
+    struct snowflake flakes[FLAKES] = {0};
+    for(int i = 0; i<FLAKES; i++) {
+        flakes[i].y = 0;
+        //flakes[i].y = (int)((float)4 * ((float)rand() / (float)RAND_MAX));
+        flakes[i].x = (int)((float)x_max * ((float)rand() / (float)RAND_MAX));
+        flakes[i].last_moved = (int)((float)900 * ((float)rand() / (float)RAND_MAX));
+        flakes[i].stuck = 0;
+    }
 
-        snprintf(cnt_buf, 40, "%c     frames: %4d",
-                 md_spinner[frame_cnt % (sizeof (hi_spinner) - 1)],
-                 frame_cnt++);
-        memcpy(&buf[2][8], cnt_buf, strlen(cnt_buf));
+    // snowman
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    attron(COLOR_PAIR(2));
+    mvprintw(y_max-6, 12,"_\n");
+    mvprintw(y_max-5, 10,"_[_]_\n");
+    attroff(COLOR_PAIR(2));
+    mvprintw(y_max-4, 11,     "(\")\n");
+    mvprintw(y_max-3,  7, "`--( : )--'\n");
+    mvprintw(y_max-2,  9,   "(  :  )\n");
 
-        snprintf(cnt_buf, 40, "%c    avg fps: %2.1f",
-                 lo_spinner[frame_cnt % (sizeof (lo_spinner) - 1)],
-                 (float)frame_cnt / (float)tm_diff_sec);
-//                 (double)frame_cnt / (double)tm_diff_nsec);
-//                 1e9f / (float)tm_diff_nsec);
-        memcpy(&buf[3][8], cnt_buf, strlen(cnt_buf));
 
-        for(unsigned short row = 0; row < ws.ws_row; row++) {
-            write(1, buf[row], ws.ws_col);
-            if(row+1 < ws.ws_row) {
-                fputs(LF CR "\0", stdout);
+    char spinner[] = "__--==^^^^==--__";
+    int frame_cnt = 0;
+    //float target_fps = 15;
+    while( 1 ) {
+        move(0, 0);
+        // no need for &row, &col, this is a macro.
+        getmaxyx(stdscr, y_max, x_max);
+
+        for(int i = 0; i<FLAKES; i++) {
+//            if(!flakes[i].stuck && 12 < frame_cnt - flakes[i].last_moved) {
+            if(18 < (frame_cnt - flakes[i].last_moved)) {
+                flakes[i].last_moved = frame_cnt;
+                mvaddch(flakes[i].y, flakes[i].x, ' ');
+
+                // move flake
+                float r = (float)rand() / (float)RAND_MAX;
+                if(flakes[i].y+1 < y_max && mvinch(flakes[i].y+1, flakes[i].x) == ' ') {
+                    flakes[i].y++;
+                    if     (r < 0.25) flakes[i].x = (flakes[i].x - 1) % (x_max-1);
+                    else if(r > 0.75) flakes[i].x = (flakes[i].x + 1) % (x_max-1);
+                }
+                if(mvinch(flakes[i].y+2, flakes[i].x) != '.') {
+                    mvaddch(flakes[i].y, flakes[i].x, '.');
+                }
             }
-            fflush(stdout);
         }
-        //fflush(stdout);
-        //fputs(GO_HOME, stdout);
-        nanosleep(&tm_asked, &tm_left);
+
+        // snowflakes
+        // if(12 < frame_cnt - last_moved) {
+        //     last_moved = frame_cnt;
+        //     // erase old snowflake position
+        //     mvaddch(fl_y, fl_x, ' ');
+
+            // // move flake
+            // float r = (float)rand() / (float)RAND_MAX;
+            // if(fl_y+1 < y_max && mvinch(fl_y+1, fl_x) == ' ') {
+            //     fl_y++;
+            //     if     (r < 0.25) fl_x = (fl_x - 1) % (x_max-1);
+            //     else if(r > 0.75) fl_x = (fl_x + 1) % (x_max-1);
+            // }
+
+        //     // draw snowflake.
+        //     mvaddch(fl_y, fl_x, '.');
+        // }
+
+        // mvprintw(3, 8, "fl_y: %d %d %d",
+        //          fl_y,
+        //          fl_y < y_max,
+        //          mvinch(fl_y+1, fl_x) != ' ');
+
+        if(y_max != y_max_old || x_max != x_max_old) {
+            // for(int y = 0; y < y_max; y++) {
+            //     //mvaddch(y, 0, '0');
+            //     //mvaddch(y, x_max-1, 'E');
+            //     //     for(int x = 0; x < x_max; x++) {
+            //     //         if(x == 0)             mvaddch(y, x, '0');
+            //     //         else if (x+1 == x_max) mvaddch(y, x, 'E');
+            //     //         else                   addch('.');
+            //     //     }
+            // };
+            init_pair(1, COLOR_GREEN, COLOR_BLACK);
+            attron(COLOR_PAIR(1));
+            for(int x = 0; x < x_max; mvaddch(y_max-1, x++, '"'));
+            attroff(COLOR_PAIR(1));
+            y_max_old = y_max;
+            x_max_old = x_max;
+            refresh();
+        }
+
+        mvprintw(2, 8, "Hello, world! %c",
+                 spinner[frame_cnt % (sizeof (spinner) - 1)]);
+        mvprintw(3, 8, "Merry Christmas!");
+        //mvprintw(3, 8, "rows: %3d, cols: %3d", y_max, x_max);
+        frame_cnt++;
+        refresh();
+        usleep(66700);
     }
 
     // CLEANUP
-    if(tcsetattr(0, TCSANOW, &old_term)) { FEPRINTF("restore_term failed"); }
-    fputs(SHOW_CURSOR, stdout);
-    fputs(RESET_TERM, stdout);
-    fflush(stdout);
+    endwin();
     return 0;
 }
--- makefile
+++ makefile
@@ -1,10 +1,18 @@
-#opt = -std=gnu17 -Wall -Wextra -Werror
-opt = -std=gnu17 -g
+#opt = -std=c11 -Wall -Wextra -Werror
+#HEADERS = foo.h bar.h
+LIBS = -lncurses
+# gcc disable usleep and nanosleep with c11 for some reason, even though
+# they are in posix, so we use gnu11 instead.
+CFLAGS = -std=gnu11 -Wall -Wextra -g
 
-all: 3t
+default: 3t
 
-3t: 3t.c
-	gcc $(opt) -o 3t 3t.c
+3t.o: 3t.c
+	gcc $(CFLAGS) -c 3t.c -o 3t.o
+
+3t: 3t.o
+	gcc $(CFLAGS) $(LIBS) 3t.o -o 3t
 
 clean:
+	-rm -f 3t.o
 	-rm -f 3t

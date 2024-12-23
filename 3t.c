36329232681dbdd46a563283aafc71223c964e4e
Adam Mullins <adam.l.m@gmail.com>
2024-12-23 07:14:16 -0500

    use ncurses, can render simple shape

--- 3t.c
+++ 3t.c
@@ -1,143 +1,251 @@
+// NB: I have conciously chosen to do precious little error handling
+// in this program. It is primarily educational. If you choose to use
+// portions of this code, do so with that in mind.
 #include <locale.h>
+#include <math.h>    // for sinf(), cosf(), tanf(), M_PI
 #include <ncurses.h>
+#include <stdbool.h>
+#include <stdlib.h>  // for abs()
 #include <unistd.h>  // for usleep()
-#include <stdlib.h>  // for rand()
-#include <time.h>    // for time()
 
-#define FLAKES 256
+#define LEN(XS) (sizeof (XS) / sizeof (XS[0]))
+
+typedef struct vec3 {
+    float x, y, z;
+} vec3;
+
+typedef struct tri {
+    vec3 p[3];
+} tri;
+
+typedef struct mat4x4 {
+    float m[4][4];
+} mat4x4;
+
+// All of these braces are to make gcc -Wmissing-braces happy.
+static const tri unit_cube[] = {
+    // SOUTH
+    {.p = {{0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 0.0f}}},
+    {.p = {{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f}}},
+    // EAST
+    {.p = {{1.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}}},
+    {.p = {{1.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}}},
+    // NORTH
+    {.p = {{1.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}}},
+    {.p = {{1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}}},
+    // WEST
+    {.p = {{0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}}},
+    {.p = {{0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 0.0f}}},
+    // TOP
+    {.p = {{0.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 1.0f, 1.0f}}},
+    {.p = {{0.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f, 0.0f}}},
+    // BOTTOM
+    {.p = {{1.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}}},
+    {.p = {{1.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}}},
+};
+
+void
+startup()
+{
+    // per the advice of `man ncurses`
+    setlocale(LC_ALL, "");
+
+    // init ncuruses
+    initscr();
+
+    // ncurses options
+    cbreak();
+    noecho();
+    curs_set(0);
+    scrollok(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+}
+
+// adapted from:
+//   https://github.com/OneLoneCoder/Javidx9/tree/master/ConsoleGameEngine
+// TODO figure out how this works or write own,
+//      refactor args to be 0 indexed.
+void
+draw_line(int x1, int y1, int x2, int y2, char c)
+{
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+
+    dx = x2 - x1; dy = y2 - y1;
+    dx1 = abs(dx); dy1 = abs(dy);
+    px = 2 * dy1 - dx1;    py = 2 * dx1 - dy1;
+
+    if (dy1 <= dx1) {
+        if (dx >= 0) {
+            x = x1; y = y1; xe = x2;
+        }
+        else {
+            x = x2; y = y2; xe = x1;
+        }
+
+        mvaddch(y, x, c);
+        //Draw(x, y, c, col);
+
+        for (i = 0; x<xe; i++) {
+            x = x + 1;
+            if (px<0) {
+                px = px + 2 * dy1;
+            }
+            else {
+                if ((dx<0 && dy<0) || (dx>0 && dy>0)) y = y + 1; else y = y - 1;
+                px = px + 2 * (dy1 - dx1);
+            }
+            mvaddch(y, x, c);
+            // Draw(x, y, c, col);
+        }
+    }
+    else {
+        if (dy >= 0) {
+            x = x1; y = y1; ye = y2;
+        }
+        else {
+            x = x2; y = y2; ye = y1;
+        }
+
+        mvaddch(y, x, c);
+        // Draw(x, y, c, col);
+
+        for (i = 0; y<ye; i++) {
+            y = y + 1;
+            if (py <= 0) {
+                py = py + 2 * dx1;
+            }
+            else {
+                if ((dx<0 && dy<0) || (dx>0 && dy>0)) x = x + 1; else x = x - 1;
+                py = py + 2 * (dx1 - dy1);
+            }
+            mvaddch(y, x, c);
+            // Draw(x, y, c, col);
+        }
+    }
+}
+
+void
+draw_tri(const tri *t, char c)
+{
+    draw_line(t->p[0].x, t->p[0].y, t->p[1].x, t->p[1].y, c);
+    draw_line(t->p[1].x, t->p[1].y, t->p[2].x, t->p[2].y, c);
+    draw_line(t->p[2].x, t->p[2].y, t->p[0].x, t->p[0].y, c);
+}
+
+// adapted from:
+//   https://github.com/OneLoneCoder/Javidx9/tree/master/ConsoleGameEngine
+void
+mul_mat_vec(const mat4x4 *m, const vec3 *i, vec3 *o) {
+    float w;
+    o->x = i->x * m->m[0][0] + i->y * m->m[1][0] + i->z * m->m[2][0] + m->m[3][0];
+    o->y = i->x * m->m[0][1] + i->y * m->m[1][1] + i->z * m->m[2][1] + m->m[3][1];
+    o->z = i->x * m->m[0][2] + i->y * m->m[1][2] + i->z * m->m[2][2] + m->m[3][2];
+       w = i->x * m->m[0][3] + i->y * m->m[1][3] + i->z * m->m[2][3] + m->m[3][3];
+
+    if (w != 0.0f) {
+	o->x /= w;
+        o->y /= w;
+        o->z /= w;
+    }
+}
 
 int
 main()
 {
-    // seed random
-    srand(time(NULL));
-    // set locale, per the advice of `man ncurses`. by passing "" as
-    // second arg, we follow whatever is set by the env.
-    setlocale(LC_ALL, "");
+    startup();
 
-    // INITIALIZE NCURSES
-    initscr();
-    start_color();
+    // PERSPECTIVE TRANSFORM
+    float near = 0.1f;
+    float far = 1000.0f;
+    float fov = 90.0f;
 
-    // cbreak disables line-buffering, input is available immediately.
-    cbreak();
-    noecho();
-    // hide cursor
-    curs_set(0);
-    // do not scroll if the cursor falls off the last line.
-    scrollok(stdscr, FALSE);
-    // if TRUE, flush on interrupt. Feels faster, but confuses
-    // ncurses, according to man cur_inopts.
-    intrflush(stdscr, FALSE);
-    keypad(stdscr, TRUE);
-    // nodelay makes getch() nonblocking, which will want later.
-    // nodelay(stdscr, TRUE);
+    mat4x4 rot_z = {0};
+    mat4x4 rot_x = {0};
 
-    int y_max, x_max, y_max_old, x_max_old;
+    int y_max, x_max;
     getmaxyx(stdscr, y_max, x_max);
-    y_max_old = 0;
-    x_max_old = 0;
-
-    // snowflakes
-    struct snowflake {
-        int x;
-        int y;
-        int last_moved;
-        int stuck;
-    };
-    struct snowflake flakes[FLAKES] = {0};
-    for(int i = 0; i<FLAKES; i++) {
-        flakes[i].y = 0;
-        //flakes[i].y = (int)((float)4 * ((float)rand() / (float)RAND_MAX));
-        flakes[i].x = (int)((float)x_max * ((float)rand() / (float)RAND_MAX));
-        flakes[i].last_moved = (int)((float)900 * ((float)rand() / (float)RAND_MAX));
-        flakes[i].stuck = 0;
-    }
-
-    // snowman
-    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-    attron(COLOR_PAIR(2));
-    mvprintw(y_max-6, 12,"_\n");
-    mvprintw(y_max-5, 10,"_[_]_\n");
-    attroff(COLOR_PAIR(2));
-    mvprintw(y_max-4, 11,     "(\")\n");
-    mvprintw(y_max-3,  7, "`--( : )--'\n");
-    mvprintw(y_max-2,  9,   "(  :  )\n");
-
-
     char spinner[] = "__--==^^^^==--__";
-    int frame_cnt = 0;
+    unsigned long long frame_cnt = 0;
     //float target_fps = 15;
+
     while( 1 ) {
-        move(0, 0);
-        // no need for &row, &col, this is a macro.
         getmaxyx(stdscr, y_max, x_max);
+        move(0, 0);
+        erase();
 
-        for(int i = 0; i<FLAKES; i++) {
-//            if(!flakes[i].stuck && 12 < frame_cnt - flakes[i].last_moved) {
-            if(18 < (frame_cnt - flakes[i].last_moved)) {
-                flakes[i].last_moved = frame_cnt;
-                mvaddch(flakes[i].y, flakes[i].x, ' ');
+        // Transform needs to be recalculated in case the window size changes.
+        // The 2x coeff to the aspect ratio is to corret for the fact that
+        // characters are not square.
+        float aspect = 2 * ((float)y_max / (float)x_max);
+        float fov_rad = 1 / tanf(fov * 0.5f / 180.0f * (float)M_PI);
+        mat4x4 mat_proj = { .m = {
+            {aspect * fov_rad, 0.0f,    0.0f,                       0.0f},
+            {0.0f,             fov_rad, 0.0f,                       0.0f},
+            {0.0f,             0.0f,    far / (far - near),         1.0f},
+            {0.0f,             0.0f,    (-far*near) / (far - near), 0.0f}
+        }};
 
-                // move flake
-                float r = (float)rand() / (float)RAND_MAX;
-                if(flakes[i].y+1 < y_max && mvinch(flakes[i].y+1, flakes[i].x) == ' ') {
-                    flakes[i].y++;
-                    if     (r < 0.25) flakes[i].x = (flakes[i].x - 1) % (x_max-1);
-                    else if(r > 0.75) flakes[i].x = (flakes[i].x + 1) % (x_max-1);
-                }
-                if(mvinch(flakes[i].y+2, flakes[i].x) != '.') {
-                    mvaddch(flakes[i].y, flakes[i].x, '.');
-                }
-            }
+        float theta = (float)frame_cnt / 15.0 / (0.5*M_PI);
+
+        rot_z.m[0][0] = cosf(theta);
+        rot_z.m[0][1] = sinf(theta);
+        rot_z.m[1][0] = -sinf(theta);
+        rot_z.m[1][1] = cosf(theta);
+        rot_z.m[2][2] = 1;
+        rot_z.m[3][3] = 1;
+
+        rot_x.m[0][0] = 1;
+        rot_x.m[1][1] = cosf(theta * 0.5f);
+        rot_x.m[1][2] = sinf(theta * 0.5f);
+        rot_x.m[2][1] = -sinf(theta * 0.5f);
+        rot_x.m[2][2] = cosf(theta * 0.5f);
+        rot_x.m[3][3] = 1;
+
+        // Draw unit cube.
+        for(size_t i = 0; i < LEN(unit_cube); i++) {
+            // we must use seperate vars for each input and output,
+            // because mul_mat_vec assumes the input vector doesn't
+            // change.
+            tri t, projected, translated, rotated_z, rotated_zx;
+            t = unit_cube[i];
+
+            mul_mat_vec(&rot_z, &t.p[0], &rotated_z.p[0]);
+            mul_mat_vec(&rot_z, &t.p[1], &rotated_z.p[1]);
+            mul_mat_vec(&rot_z, &t.p[2], &rotated_z.p[2]);
+
+            mul_mat_vec(&rot_x, &rotated_z.p[0], &rotated_zx.p[0]);
+            mul_mat_vec(&rot_x, &rotated_z.p[1], &rotated_zx.p[1]);
+            mul_mat_vec(&rot_x, &rotated_z.p[2], &rotated_zx.p[2]);
+
+            //translated = unit_cube[i];
+            translated = rotated_zx;
+            translated.p[0].z += 2.0f;
+            translated.p[1].z += 2.0f;
+            translated.p[2].z += 2.0f;
+
+            // Apply perspective transform to each point.
+            mul_mat_vec(&mat_proj, &translated.p[0], &projected.p[0]);
+            mul_mat_vec(&mat_proj, &translated.p[1], &projected.p[1]);
+            mul_mat_vec(&mat_proj, &translated.p[2], &projected.p[2]);
+
+            // Each point has a range of -1 to +1, so it must be
+            // scaled into screen space.
+            projected.p[0].x += 1.0f; projected.p[0].y += 1.0f;
+            projected.p[1].x += 1.0f; projected.p[1].y += 1.0f;
+            projected.p[2].x += 1.0f; projected.p[2].y += 1.0f;
+
+            projected.p[0].x *= 0.5f * x_max; projected.p[0].y *= 0.5f * y_max;
+            projected.p[1].x *= 0.5f * x_max; projected.p[1].y *= 0.5f * y_max;
+            projected.p[2].x *= 0.5f * x_max; projected.p[2].y *= 0.5f * y_max;
+
+            draw_tri(&projected, 'O');
         }
 
-        // snowflakes
-        // if(12 < frame_cnt - last_moved) {
-        //     last_moved = frame_cnt;
-        //     // erase old snowflake position
-        //     mvaddch(fl_y, fl_x, ' ');
+        mvprintw(2, 8, "Hello, world! %c", spinner[frame_cnt % (sizeof (spinner) - 1)]);
+        mvprintw(4, 9, "win size: %d col, %d row", x_max, y_max);
+        mvprintw(5, 8, "frame_cnt: %lld", frame_cnt);
+        mvprintw(6, 12, "theta: %04.2f * pi", (float)theta / M_PI);
 
-            // // move flake
-            // float r = (float)rand() / (float)RAND_MAX;
-            // if(fl_y+1 < y_max && mvinch(fl_y+1, fl_x) == ' ') {
-            //     fl_y++;
-            //     if     (r < 0.25) fl_x = (fl_x - 1) % (x_max-1);
-            //     else if(r > 0.75) fl_x = (fl_x + 1) % (x_max-1);
-            // }
-
-        //     // draw snowflake.
-        //     mvaddch(fl_y, fl_x, '.');
-        // }
-
-        // mvprintw(3, 8, "fl_y: %d %d %d",
-        //          fl_y,
-        //          fl_y < y_max,
-        //          mvinch(fl_y+1, fl_x) != ' ');
-
-        if(y_max != y_max_old || x_max != x_max_old) {
-            // for(int y = 0; y < y_max; y++) {
-            //     //mvaddch(y, 0, '0');
-            //     //mvaddch(y, x_max-1, 'E');
-            //     //     for(int x = 0; x < x_max; x++) {
-            //     //         if(x == 0)             mvaddch(y, x, '0');
-            //     //         else if (x+1 == x_max) mvaddch(y, x, 'E');
-            //     //         else                   addch('.');
-            //     //     }
-            // };
-            init_pair(1, COLOR_GREEN, COLOR_BLACK);
-            attron(COLOR_PAIR(1));
-            for(int x = 0; x < x_max; mvaddch(y_max-1, x++, '"'));
-            attroff(COLOR_PAIR(1));
-            y_max_old = y_max;
-            x_max_old = x_max;
-            refresh();
-        }
-
-        mvprintw(2, 8, "Hello, world! %c",
-                 spinner[frame_cnt % (sizeof (spinner) - 1)]);
-        mvprintw(3, 8, "Merry Christmas!");
-        //mvprintw(3, 8, "rows: %3d, cols: %3d", y_max, x_max);
         frame_cnt++;
         refresh();
         usleep(66700);
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 #opt = -std=c11 -Wall -Wextra -Werror
 #HEADERS = foo.h bar.h
-LIBS = -lncurses
+LIBS = -lncurses -lm
 # gcc disable usleep and nanosleep with c11 for some reason, even though
 # they are in posix, so we use gnu11 instead.
 CFLAGS = -std=gnu11 -Wall -Wextra -g
